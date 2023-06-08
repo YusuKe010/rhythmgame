3a4afa845c0ae2c4901fd86c4405499cfb1711d1
YusuKe010 <yuusuke.k1717@gmail.com>
2023-06-08 12:51:18 +0900

    ブロックと判定作った

--- Assets/script/Block.cs
+++ Assets/script/Block.cs
@@ -7,7 +7,7 @@ using UnityEngine.UI;
 public class Block : MonoBehaviour
 {
     
-    public int _score = 0;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -17,26 +17,15 @@ public class Block : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(gameObject.transform.position.y < -15f)
-        {
-            Destroy(gameObject);
-        }
+       
     }
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.CompareTag("greet")&& Input.GetKey(KeyCode.A)) 
+        if (collision.gameObject.CompareTag("greet") ||collision.gameObject.CompareTag("parfect") && Input.GetKey(KeyCode.A))
         {
-            Debug.Log("greet");
-            _score+=100;
-            
             gameObject.SetActive(false);
         }
-        if (collision.gameObject.CompareTag("parfect") && Input.GetKey(KeyCode.A))
-        {
-            Debug.Log("parfect");
-            _score += 10000;
-           
-            gameObject.SetActive(false);
-        }
+       
     }
+
 }
--- Assets/script/Score.cs
+++ Assets/script/Score.cs
@@ -6,20 +6,36 @@ using UnityEngine.UI;
 public class Score : MonoBehaviour
 {
     [SerializeField] Text _text;
-    [SerializeField] Block _block;
+    [SerializeField] Text _conbo;
+    public int _score = 0;
+    int conbo = 0;
     // Start is called before the first frame update
     public void Start()
     {
-        Block _blockscore = GetComponent<Block>();
-        
+       
     }
 
     // Update is called once per frame
     public void Update()
     {
+        
+        _text.text = _score.ToString("00");
+        _conbo.text = conbo.ToString("0");
+    }
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("block") && Input.GetKey(KeyCode.A))
+        {
+           
+            _score += 100;
+            conbo += 1;
 
-        _text.text = _block._score.ToString("00");
+        }
 
     }
-
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        conbo = 0;
+        Destroy(collision.gameObject);
+    }
 }
